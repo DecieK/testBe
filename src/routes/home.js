12900ae0e308a59e88807fe301e94dffb38005a5
deciek <luongvukhoa572001@gmail.com>
2023-11-03 02:37:08 +0700

    4

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,27 +3,20 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
     return new Promise(async (resolve, reject) => {
-        try {
-          let chitietve = "";
-          if (id_ve === "ALL") {
-            chitietve = await db.chitietves.findAll({
-            });
-          }
-    
-          if (id_ve && id_ve !== "ALL") {
-            chitietve = await db.chitietves.findAll({
-              where: { id_Ve: id_ve },
-            });
-          }
-          resolve(chitietve);
-        } catch (e) {
-          reject(e);
-        }
-      });
-//   return res.status(200).json({
-//     title: "Express Testing",
-//     message: "The app is working properly!",
-//   });
+
+
+
+        chitietve = await db.chitietves.findAll({
+            where: { id_Ve: id_ve },
+        });
+
+        resolve(chitietve);
+
+    });
+    //   return res.status(200).json({
+    //     title: "Express Testing",
+    //     message: "The app is working properly!",
+    //   });
 });
 
 module.exports = router;
