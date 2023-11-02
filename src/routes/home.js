676f3b99f5b33c0962b73ca868959a2f646864d2
deciek <luongvukhoa572001@gmail.com>
2023-11-03 02:43:45 +0700

    6

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const db = {};
 
 router.get("/", async (req, res, next) => {
+    let  chitietv = await db.chitietves.findAll({})
+
     return res.status(200).json({
 
-        chitietve: await db.chitietves.findAll({}),
+
+        chitietv
 
 
-
-
-    });
+});
     //   return res.status(200).json({
     //     title: "Express Testing",
     //     message: "The app is working properly!",
