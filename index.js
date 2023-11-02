d9d54c50146e48c6b31097e55d2b924ff7b83b17
deciek <luongvukhoa572001@gmail.com>
2023-11-02 14:36:56 +0700

    12

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 const express = require("express");
 const home = require("./src/routes/home");
 import configViewEngine from "./src/configs/viewEngine";
-// import initWebRoutes from "./src/routes/web";
-import connectDB from "./src/configs/connectDB";
+import initWebRoutes from "./src/routes/web";
+// import connectDB from "./src/configs/connectDB";
 // Middlewares
 const app = express();
 app.use(express.json());
 configViewEngine(app);
 // //init web route (dieu huong website)
-// initWebRoutes(app);
+initWebRoutes(app);
 // //connectDatabase
-connectDB();
+// connectDB();
 // Routes
 app.use("/home", home);
 
