20e0e3cb27107fcb69f043f46f59ec8b7ab41caa
deciek <luongvukhoa572001@gmail.com>
2023-11-03 12:55:50 +0700

    13

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-// Import packages
-const express = require("express");
-const home = require("./src/routes/home");
-const web = require("./src/routes/web")
-// import configViewEngine from "./src/configs/viewEngine";
-// import initWebRoutes from "./src/routes/web";
-// import connectDB from "./src/configs/connectDB";
-// Middlewares
+// //Import packages
+// const express = require("express");
+// const home = require("./src/routes/home");
+// const web = require("./src/routes/web")
+// // import configViewEngine from "./src/configs/viewEngine";
+// // import initWebRoutes from "./src/routes/web";
+// // import connectDB from "./src/configs/connectDB";
+// // Middlewares
+// const app = express();
+// app.use(express.json());
+// // configViewEngine(app);
+// // //init web route (dieu huong website)
+// // initWebRoutes(app);
+// // //connectDatabase
+// // connectDB();
+// // Routes\
+// app.use("/a",web)
+// app.use("/home", home);
+
+// // connection
+// const port = process.env.PORT || 9001;
+// app.listen(port, () => console.log(`Listening to port ${port}`));
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
-// configViewEngine(app);
-// //init web route (dieu huong website)
-// initWebRoutes(app);
-// //connectDatabase
-// connectDB();
-// Routes\
-app.use("/a",web)
+const port = process.env.PORT || 3000;
+const programmingLanguagesRouter = require('./src/routes/programmingLanguages.route');
+const home = require("./src/routes/home");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({'message': 'ok'});
+})
 app.use("/home", home);
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+app.use('/programming-languages', programmingLanguagesRouter);
 
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const db = {};
-const chitietve = require('../controller/userController');
+// const db = {};
+// const chitietve = require('../controller/userController');
 
 router.get("/", async (req, res, next) => {
-    try {
-        res.json(await chitietve.getMultiple(req.query.idctv));
-    } catch (err) {
-        console.error(`Error while getting programming languages `, err.message);
-        next(err);
-    }
+    // try {
+    //   return  res.json(await chitietve.getMultiple(req.query.idctv));
+    // } catch (err) {
+    //     console.error(`Error while getting programming languages `, err.message);
+    //     next(err);
+    // }
 
 
 
-    //   return res.status(200).json({
-    //     title: "Express Testing",
-    //     message: "The app is working properly!",
-    //   });
+      return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+      });
 });
 
 module.exports = router;
