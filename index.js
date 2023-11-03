7b8aaefac8641ccc09d4c0ff789c1e541e8ec1a1
deciek <luongvukhoa572001@gmail.com>
2023-11-03 14:19:34 +0700

    17

--- index.js
+++ index.js
@@ -20,23 +20,38 @@
 // // connection
 // const port = process.env.PORT || 9001;
 // app.listen(port, () => console.log(`Listening to port ${port}`));
+import cors from "cors";
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+// const app = express();
 const port = process.env.PORT || 3000;
-// const programmingLanguagesRouter = require('./src/routes/web.js');
-const home = require("./src/routes/home");
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const initWebRoutes = require('./src/routes/web.js');
+const configViewEngine = require('./src/configs/viewEngine.js');
+const connectDB = require('./src/configs/connectDB.js');
+
+const home = require("./src/routes/home");
+require("dotenv").config();
+const corsConfig = {
+    credentials: true,
+    origin: true,
+}
+
+const app = express();
+app.use(cors(corsConfig));
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+configViewEngine(app);
+initWebRoutes(app);
+connectDB();
 
 app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
+    res.json({ 'message': 'ok' });
 })
 app.use("/home", home);
 
@@ -44,13 +59,13 @@ app.use("/home", home);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-  
-  return;
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ 'message': err.message });
+
+    return;
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 });
